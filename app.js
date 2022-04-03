539167f9403c260185cafc12c5d89ec2a548c580
dpertierra <dpertierra@hotmail.com>
2022-04-03 02:08:22 -0300

    Logica para cambiar de tab

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-app.use(express.static(path.resolve("public")));
+app.use(express.static(path.resolve(__dirname, "public")));
 
 app.listen(3000, () => console.log("Corriendo en http://localhost:3000"));
 
-app.get("/", (req, res) => res.sendFile(path.resolve("views/home.html")));
+app.get("/", (req, res) =>
+	res.sendFile(path.resolve(__dirname, "views/home.html")),
+);
---
+++ public/js/script.js
@@ -0,0 +1,29 @@
+// Get all the tabs
+const tabs = document.querySelectorAll(".tab");
+
+tabs.forEach((clickedTab) => {
+	// Add onClick event listener on each tab
+	clickedTab.addEventListener("click", () => {
+		console.log(clickedTab);
+		tabs.forEach((tab) => {
+			if (clickedTab.parentNode.parentNode == tab.parentNode.parentNode)
+				tab.classList.remove("active");
+		});
+
+		// Add the active class on the clicked tab
+		clickedTab.classList.add("active");
+        //Ocultar habitaciones cuando la tab de 
+        //actividades este activa y viceversa
+		if (clickedTab.id == "activitiestab") {
+			activities = document.querySelector("#activities");
+			activities.classList.remove("hidden");
+			rooms = document.querySelector("#rooms");
+			rooms.classList.add("hidden");
+		} else if (clickedTab.id == "roomstab") {
+			activities = document.querySelector("#activities");
+			activities.classList.add("hidden");
+			rooms = document.querySelector("#rooms");
+			rooms.classList.remove("hidden");
+		}
+	});
+});
