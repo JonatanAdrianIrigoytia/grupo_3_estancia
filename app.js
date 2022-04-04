1a7db9193314c882707f1034c441c2dbb0f62f24
dpertierra <dpertierra@hotmail.com>
2022-04-04 02:06:08 -0300

    Agrega rutas de express para las paginas de login y registro

--- app.js
+++ app.js
@@ -9,3 +9,11 @@ app.listen(3000, () => console.log("Corriendo en http://localhost:3000"));
 app.get("/", (req, res) =>
 	res.sendFile(path.resolve(__dirname, "views/index.html")),
 );
+
+app.get("/login", (req, res) =>
+	res.sendFile(path.resolve(__dirname, "views/login.html")),
+);
+
+app.get("/register", (req, res) =>
+	res.sendFile(path.resolve(__dirname, "views/register.html")),
+);
