0a5f082d0b0fd5c7af37701f88b7ee0938a8765b
dpertierra <dpertierra@hotmail.com>
2022-04-16 18:28:54 -0300

    Fixes header logo for different screen sizes

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 
 app.use(express.static(path.resolve(__dirname, "public")));
 
-app.listen(3001, () => console.log("Corriendo en http://localhost:3000"));
+app.listen(3000, () => console.log("Corriendo en http://localhost:3000"));
 
 app.get("/", (req, res) =>
 	res.sendFile(path.resolve(__dirname, "views/index.html")),
