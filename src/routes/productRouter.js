493fd241b31766f40e7cc1f08ca8632c743f1348
dpertierra <dpertierra@hotmail.com>
2022-05-05 22:11:23 -0300

    Instalacion y configuracion de Multer

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -1,6 +1,47 @@
 const express = require("express");
 const productController = require("../controllers/productController");
 const router = express.Router();
+const multer = require("multer");
+
+// ************ Multer Configuration ************
+
+const storage = multer.diskStorage({
+	// La idea de la logica extra en el metodo destination es que si el producto que se esta creando es una habitacion
+	// La imagen se guarde en /images/rooms pero si es una actividad se guarde en /images/activities
+	// En caso de que esto no funcione la solucion seria luego de que se guarde la imagen moverla a la carpeta que corresponda
+	destination: (req, file, cb) => {
+		let dest = "../../public/images/";
+		if (req.body.category == "room") dest = "../../public/images/rooms";
+		else if (req.body.category == "activity")
+			dest = "../../public/images/activities";
+		cb(null, path.resolve(__dirname, dest));
+	},
+	filename: (req, file, cb) => {
+		let filename = getFileName(req.body);
+		cb(null, filename);
+	},
+});
+
+// const activityStorage = multer.diskStorage({
+// 	destination: (req, file, cb) =>
+// 		cb(null, path.resolve(__dirname, "../../public/images/activities")),
+// 	filename: (req, file, cb) => {
+// 		let filename = getFileName(req.body);
+// 		cb(null, filename);
+// 	},
+// });
+
+const uploads = multer({ storage });
+
+function getFileName(body) {
+	return (
+		"img-" +
+		body.name.toLowerCase().replace(/\s/g, "-") +
+		"_" +
+		Date.now() +
+		path.extname(file.originalname)
+	);
+}
 
 router.get("/", productController.list);
 router.get("/rooms", productController.listRooms);
@@ -8,8 +49,8 @@ router.get("/activities", productController.listActivities);
 router.get("/detail/", productController.detail);
 router.get("/cart", productController.cart);
 router.get("/create", productController.create);
-router.post("/create", productController.save);
+router.post("/create", uploads.single("product-image"), productController.save);
 router.get("/edit/:id", productController.edit);
-router.put("/edit", productController.save);
+router.put("/edit", uploads.single("product-image"), productController.save);
 
 module.exports = router;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -1,10 +1,28 @@
 const express = require("express");
 const userController = require("../controllers/userController");
 const router = express.Router();
+const multer = require("multer");
+
+// ************ Multer Configuration ************\
+const storage = multer.diskStorage({
+	destination: (req, file, cb) =>
+		cb(null, path.resolve(__dirname, "../../public/images/users")),
+	filename: (req, file, cb) => {
+		let filename =
+			"img-" +
+			req.body.name.toLowerCase().replace(/\s/g, "-") +
+			req.body.lastName.toLowerCase().replace(/\s/g, "-") +
+			"_" +
+			Date.now() +
+			path.extname(file.originalname);
+		cb(null, filename);
+	},
+});
+const uploads = multer({ storage });
 
 router.get("/login", userController.renderLogin);
 router.get("/register", userController.renderRegister);
 router.post("/login", userController.login);
-router.post("/register", userController.register);
+router.post("/register", uploads.single("user-image"), userController.register);
 
 module.exports = router;
