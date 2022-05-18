adf2cb1e3e69b65931e35e9cfea441a4e004849f
dpertierra <dpertierra@hotmail.com>
2022-05-18 20:04:35 -0300

    Agregando enters para mejorar la legibilidad del codigo

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,33 +2,41 @@ const path = require("path");
 const fs = require("fs");
 const Product = {
 	filepath: path.resolve(__dirname, "../data/products.json"),
+
 	getData: function () {
 		return JSON.parse(fs.readFileSync(this.filepath, "utf-8"));
 	},
+
 	findAll: function () {
 		return this.getData();
 	},
+
 	generateID: function (products = undefined) {
 		if (!products) products = this.findAll();
 		let id = 1;
 		if (products.length > 0) id = products.at(-1).id + 1;
 		return id;
 	},
+
 	findByID: function (id) {
 		return this.findAll().find((product) => product.id == id);
 	},
+
 	findByField: function (field, value) {
 		let products = this.findAll();
 		return products.find((product) => product[field] == value);
 	},
+
 	findIndexByID: function (id, products = undefined) {
 		if (!products) products = this.findAll();
 		let productIndex = products.findIndex((product) => product.id == id);
 		return productIndex;
 	},
+
 	findByCategory: function (category) {
 		return this.findAll().filter((product) => product.category == category);
 	},
+
 	create: function (productData) {
 		let products = this.findAll();
 		let id = this.generateID(products);
@@ -36,6 +44,7 @@ const Product = {
 		products.push(product);
 		fs.writeFileSync(this.filepath, JSON.stringify(products), null, " ");
 	},
+
 	edit: function (id, productData) {
 		let editedProduct = fillProductData(id, productData);
 		let products = this.findAll();
@@ -43,19 +52,23 @@ const Product = {
 		products[index] = editedProduct;
 		fs.writeFileSync(this.filepath, JSON.stringify(products, null, " "));
 	},
+
 	delete: function (id) {
 		let products = this.findAll();
 		let index = this.findIndexByID(id, products);
 		products.splice(index, 1);
 		fs.writeFileSync(this.filepath, JSON.stringify(products), null, " ");
 	},
+
 	fillProductData: function (id, productData, filename) {
 		let product = {
 			id: id,
 			...productData,
 			image: this.getFile(productData.category, filename),
 		};
+
 		product.discount = productData.discount || 0;
+
 		if (productData.category == "room") {
 			product.capacity = productData.capacity || 0;
 			product.services = productData.services ? [productData.services] : [];
@@ -63,6 +76,7 @@ const Product = {
 
 		return product;
 	},
+
 	getFilePath: function (category, filename) {
 		if (filename) {
 			if (category == "room") return "/products/rooms/" + filename;
