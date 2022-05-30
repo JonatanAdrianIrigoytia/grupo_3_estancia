37881d6d7b9874247de903907732843f4773a214
Diego Pertierra <dpertierra@hotmail.com>
2022-05-30 17:51:53 -0300

    helper function para convertir tiempo a milisegundos

---
+++ src/helpers/timeConverter.js
@@ -0,0 +1,18 @@
+function timeConverter(amount, type) {
+	switch (type) {
+		case "seconds":
+			//     1 segundo * cantidad
+			return 1000 * aumount;
+		case "minutes":
+			//      1 minuto * cantidad
+			return 1000 * 60 * amount;
+		case "hours":
+			//        1 hora      * cantidad
+			return 1000 * 60 * 60 * amount;
+		case "days":
+			//           1   dia       * cantidad
+			return 1000 * 60 * 60 * 24 * amount;
+	}
+}
+
+module.exports = timeConverter;
