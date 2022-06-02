679bd343ac1bdd071778a190993ed7d4d7ab6aff
Diego Pertierra <dpertierra@hotmail.com>
2022-06-02 16:12:28 -0300

    Agregando validaciones para ocultar campos y botones

--- src/middlewares/adminMiddleware.js
+++ src/middlewares/adminMiddleware.js
@@ -1,7 +1,7 @@
 function adminMiddleware(req, res, next) {
 	if (req.session.loggedUser && req.session.loggedUser.category == "admin")
 		next();
-	return res.redirect("/users/login");
+	res.redirect("/users/login");
 }
 
 module.exports = adminMiddleware;
