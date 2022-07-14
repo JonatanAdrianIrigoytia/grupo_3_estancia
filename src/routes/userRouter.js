1d4a78798ff3cb842bd9fe7c98da70d969453593
dpertierra <dpertierra@hotmail.com>
2022-07-14 20:50:26 -0300

    Corrigiendo nombre del input

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -15,7 +15,7 @@ const storage = multer.diskStorage({
 	filename: (req, file, cb) => {
 		let filename =
 			"img-" +
-			req.body.name.toLowerCase().replace(/\s/g, "-") +
+			req.body.firstName.toLowerCase().replace(/\s/g, "-") +
 			req.body.lastName.toLowerCase().replace(/\s/g, "-") +
 			"_" +
 			Date.now() +
