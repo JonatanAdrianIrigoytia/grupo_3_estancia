c3a826bffa195e6056eb7cceb16b07b91d5d4705
dpertierra <dpertierra@hotmail.com>
2022-07-19 20:39:07 -0300

    Correcciones login

--- public/js/login.js
+++ public/js/login.js
@@ -1,25 +1,19 @@
 import { checkIfEmailExists } from "./util.js";
 window.addEventListener("load", function () {
 	const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-	const lowerCase = /[a-z]/g;
-	const upperCase = /[A-Z]/g;
-	const number = /[0-9]/g;
-	const specialChar = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/g;
-	const validFileTypes = ["image/jpeg", "image/png", "image/jpg", "image/gif"];
 
 	let form = document.querySelector("#registerForm");
 	//Campos
-	
+
 	let email = document.getElementById("email");
 	let password = document.getElementById("password");
-	
+
 	//Errores
-	
+
 	let emailErrors = document.getElementById("emailErrors");
 
 	//Errores de contraseña
 	let passwordErrors = document.getElementById("passwordErrors");
-	let lengthError = document.getElementById("lengthError");
 
 	function checkEmail() {
 		if (email.value.length >= 2 && email.value.match(emailFormat)) {
@@ -36,38 +30,24 @@ window.addEventListener("load", function () {
 	}
 	function checkPassword() {
 		if (password.value.length >= 8) {
-			lengthError.classList.remove("invalid");
-			lengthError.classList.add("valid");
-			passwordErrorArray[4] = false;
-		} else {
-			lengthError.classList.remove("valid");
-			lengthError.classList.add("invalid");
-			passwordErrorArray[4] = true;
-		}
-
-		if (passwordErrorArray.every((predicate) => predicate === false)) {
 			password.classList.add("input-valid");
 			password.classList.remove("input-error");
-			passwordErrors.classList.add("hidden");
 			return true;
 		} else {
 			password.classList.remove("input-valid");
 			password.classList.add("input-error");
-			passwordErrors.classList.remove("hidden");
+			passwordErrors.innerHTML = "<li>Debe ingresar una contraseña</li>";
 			return false;
 		}
 	}
-    email.oninput = () => {
+	email.oninput = () => {
 		checkEmail();
 	};
 
 	password.onfocus = () => {
-		if (!passwordErrorArray.every((predicate) => predicate === false)) {
-			checkPassword();
-			passwordErrors.classList.remove("hidden");
-		}
+		checkPassword();
 	};
-    form.onsubmit = async (event) => {
+	form.onsubmit = async (event) => {
 		event.preventDefault();
 		let emailExists = await checkIfEmailExists();
 		if (emailExists) {
@@ -76,13 +56,7 @@ window.addEventListener("load", function () {
 			email.classList.remove("input-valid");
 			email.classList.add("input-error");
 		}
-        if (
-			
-			checkEmail() &&
-			!emailExists &&
-			checkPassword()
-			
-		) {
+		if (checkEmail() && !emailExists && checkPassword()) {
 			form.submit();
 		}
 	};
--- public/js/util.js
+++ public/js/util.js
@@ -10,3 +10,21 @@ export async function checkIfEmailExists() {
 	let { emailExists } = await response.json();
 	return emailExists;
 }
+
+const validFileTypes = ["image/jpeg", "image/png", "image/jpg", "image/gif"];
+export function checkImage(imageInput, imageErrors) {
+	if (imageInput.files.length > 0) {
+		if (validFileTypes.includes(imageInput.files[0].type)) {
+			imageInput.classList.remove("input-error");
+			imageInput.classList.add("input-valid");
+			imageErrors.innerHTML = "";
+			return true;
+		} else {
+			imageInput.classList.remove("input-valid");
+			imageInput.classList.add("input-error");
+			imageErrors.innerHTML =
+				"<li>El formato de la imagen es invalido, los tipos permitidos son (JPG, JPEG, PNG, GIF)</li>";
+			return false;
+		}
+	}
+}
